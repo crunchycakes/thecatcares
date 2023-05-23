a19ad4ebbc30f006a2395e2b8616fd5bfd17537e
crunchycakes <85715474+crunchycakes@users.noreply.github.com>
2023-05-22 23:07:18 -0600

    player anim

--- the-cat-cares/Assets/Scripts/PlayerMovement.cs
+++ the-cat-cares/Assets/Scripts/PlayerMovement.cs
@@ -6,18 +6,45 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private Rigidbody2D body;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    private float dirX = 0f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown("space")) {
-            body.velocity = new Vector2(0, 14);
+        dirX = Input.GetAxisRaw("Horizontal");
+        body.velocity = new Vector2(dirX * moveSpeed, body.velocity.y);
+
+
+        if (Input.GetButtonDown("Jump")) {
+            body.velocity = new Vector2(body.velocity.x, jumpForce);
+        }
+
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState() {
+        if (dirX > 0f) {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        } else if (dirX < 0f) {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        } else {
+            anim.SetBool("running", false);
         }
     }
+
 }
