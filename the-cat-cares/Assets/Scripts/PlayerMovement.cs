1448d53f18be09ed986fceba6641dc641ae08776
crunchycakes <85715474+crunchycakes@users.noreply.github.com>
2023-05-22 22:35:36 -0600

    player movement optimise

--- the-cat-cares/Assets/Scripts/PlayerMovement.cs
+++ the-cat-cares/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    private Rigidbody2D body;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space")) {
+            body.velocity = new Vector2(0, 14);
+        }
     }
 }
