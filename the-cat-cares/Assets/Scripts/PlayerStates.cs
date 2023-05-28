7bbf29c2bcf622bf31f8128aecf5e929d1b336ae
crunchycakes <85715474+crunchycakes@users.noreply.github.com>
2023-05-27 19:34:49 -0600

    new player sprite

--- the-cat-cares/Assets/Scripts/PlayerStates.cs
+++ the-cat-cares/Assets/Scripts/PlayerStates.cs
@@ -4,28 +4,96 @@ using UnityEngine;
 
 public class PlayerStates : MonoBehaviour
 {
+    private Rigidbody2D body;
     private PlayerMovement playerMovement;
     private SpriteRenderer sprite;
-    private enum PlayerState { sad, happy };
+    private enum PlayerState { sad, happy, excited };
+    private PlayerState playerState = PlayerState.excited;
+    private PlayerState lastPlayerState = PlayerState.sad;
 
     [SerializeField] private GameObject happyBox;
 
     // Start is called before the first frame update
     void Start()
     {
+        body = GetComponent<Rigidbody2D>();
         playerMovement = GetComponent<PlayerMovement>();
         sprite = GetComponent<SpriteRenderer>();
         //playerMovement.setHappyState(true);
     }
 
     private void Update() {
-        if (Input.GetButtonDown("Fire1")) { // this is "i", or left mouse button
-            float facing = 1f;
-            if (sprite.flipX) {
-                facing = -1f;
-            }
-            Instantiate(happyBox, transform.position + new Vector3(0.5f * facing, 0f, 0f), Quaternion.identity);
+
+        // handle state changes
+        // switching to happy state
+        if (playerState == PlayerState.happy && lastPlayerState != PlayerState.happy) {
+            playerMovement.setHappyState(true);
+        } else if (playerState != PlayerState.happy && lastPlayerState == PlayerState.happy) {
+            playerMovement.setHappyState(false);
         }
+
+        // handle pressing fire key
+        Fire1KeyDown();
+        Fire1KeyUp();
+
+        // record last state here i guess
+        lastPlayerState = playerState;
+
+    }
+
+    private void Fire1KeyDown() {
+        if (Input.GetButtonDown("Fire1")) { // this is "i", or left mouse button
+            switch (playerState) {
+
+                case PlayerState.happy:
+                    break;
+
+                case PlayerState.sad:
+                    sadActivation();
+                    break;
+                
+                case PlayerState.excited:
+                    excitedActivation();
+                    break;
+
+            }
+        }
+    }
+
+    private void Fire1KeyUp() {
+        if (Input.GetButtonUp("Fire1")) {
+            switch (playerState) {
+
+                case PlayerState.happy:
+                    break;
+
+                case PlayerState.sad:
+                    break;
+                
+                case PlayerState.excited:
+                    excitedDeactivation();
+                    break;
+
+            }
+        }
+    }
+
+    // spawn box
+    private void sadActivation() {
+        float facing = 1f;
+        if (sprite.flipX) {
+            facing = -1f;
+        }
+        Instantiate(happyBox, transform.position + new Vector3(0.5f * facing, 0f, 0f), Quaternion.identity);
+    }
+
+    // floating behaviour
+    private void excitedActivation() {
+        body.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    private void excitedDeactivation() {
+        body.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 
 }
